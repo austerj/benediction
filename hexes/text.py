523dcbbc1a2921cbf355b5a31f3111953b800725
austerj <johanauster@gmail.com>
2024-03-24 16:50:00 +0100

    feat: simple text wrap

--- hexes/text.py
+++ hexes/text.py
@@ -15,3 +15,22 @@ def align(strs: list[str], alignment: HorizontalAlignment):
     width = max(len(s) for s in stripped_strs)
     # align each row in same width
     return _XALIGN[alignment](stripped_strs, width)
+
+
+def simple_wrap(strs: list[str], width: int) -> list[str]:
+    """Wrap list of strings into strings of length less than or equal to width."""
+    # special case: just return every char from flattened list
+    if width <= 1:
+        return [c for str_ in strs for c in str_]
+    # build up list by exhausting flattened string
+    wrapped_strs = []
+    remaining_str = "\n".join(strs)
+    while remaining_str:
+        # ignore leading whitespace after the first line
+        if wrapped_strs and (remaining_str[0] == "\n" or remaining_str[0] == " "):
+            wrapped_strs.append(remaining_str[1 : width + 1].replace("\n", " ") or " ")
+            remaining_str = remaining_str[width + 1 :]
+        else:
+            wrapped_strs.append(remaining_str[:width].replace("\n", " "))
+            remaining_str = remaining_str[width:]
+    return wrapped_strs
--- tests/test_text.py
+++ tests/test_text.py
@@ -1,8 +1,8 @@
-from hexes.text import align
+from hexes.text import align, simple_wrap
 
 test_str = [
     "  HEYY",
-    "WHATTTTT",
+    "WHAT TTT",
     "  SUP ",
 ]
 
@@ -10,18 +10,83 @@ test_str = [
 def test_left_align():
     assert align(test_str, "left") == [
         "HEYY    ",
-        "WHATTTTT",
+        "WHAT TTT",
         "SUP     ",
     ]
 
     assert align(test_str, "center") == [
         "  HEYY  ",
-        "WHATTTTT",
+        "WHAT TTT",
         "  SUP   ",
     ]
 
     assert align(test_str, "right") == [
         "    HEYY",
-        "WHATTTTT",
+        "WHAT TTT",
         "     SUP",
+    ]
+
+
+def test_simple_wrap():
+    # lines below width are merged
+    assert simple_wrap(test_str, 8) == [
+        "  HEYY W",
+        "HAT TTT ",
+        " SUP ",
+    ]
+
+    # original (extra) whitespace is respected
+    assert simple_wrap(test_str, 5) == [
+        "  HEY",
+        "Y WHA",
+        "T TTT",
+        "  SUP",
+        " ",
+    ]
+
+    assert simple_wrap(test_str, 3) == [
+        "  H",
+        "EYY",
+        "WHA",
+        "T T",
+        "TT ",
+        " SU",
+        "P ",
+    ]
+
+    assert simple_wrap(test_str, 2) == [
+        "  ",
+        "HE",
+        "YY",
+        "WH",
+        "AT",
+        "TT",
+        "T ",
+        " S",
+        "UP",
+        " ",
+    ]
+
+    # width of 1 (special case) produces sequence of individual characters
+    assert simple_wrap(test_str, 1) == [
+        " ",
+        " ",
+        "H",
+        "E",
+        "Y",
+        "Y",
+        "W",
+        "H",
+        "A",
+        "T",
+        " ",
+        "T",
+        "T",
+        "T",
+        " ",
+        " ",
+        "S",
+        "U",
+        "P",
+        " ",
     ]
