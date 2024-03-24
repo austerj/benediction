75d8590ead47a2491dfd74c2d91da2fae70b4d97
austerj <johanauster@gmail.com>
2024-03-24 17:56:31 +0100

    feat: only strip whitespace of aligning side

--- hexes/text.py
+++ hexes/text.py
@@ -11,13 +11,18 @@ _XALIGN: dict[HorizontalAlignment, typing.Callable[[list[str], int], list[str]]]
 def align(strs: list[str], alignment: HorizontalAlignment):
     """Horizontal alignment of strings."""
     # strip whitespace
-    stripped_strs = [s.strip() for s in strs]
+    if alignment == "left":
+        stripped_strs = [s.lstrip() for s in strs]
+    elif alignment == "right":
+        stripped_strs = [s.rstrip() for s in strs]
+    else:
+        stripped_strs = [s.strip() for s in strs]
     width = max(len(s) for s in stripped_strs)
     # align each row in same width
     return _XALIGN[alignment](stripped_strs, width)
 
 
-def simple_wrap(strs: str | list[str], width: int) -> list[str]:
+def simple_wrap(strs: str | list[str], width: int, ignore_leading_whitespace: bool = True) -> list[str]:
     """Wrap (list of) strings into strings of length less than or equal to width."""
     # special case: just return every char from flattened list
     if width <= 1:
@@ -29,7 +34,7 @@ def simple_wrap(strs: str | list[str], width: int) -> list[str]:
     remaining_str = strs if isinstance(strs, str) else "\n".join(strs)
     while remaining_str:
         # ignore leading whitespace after the first line
-        if wrapped_strs and (remaining_str[0] == "\n" or remaining_str[0] == " "):
+        if wrapped_strs and (remaining_str[0] == "\n" or (ignore_leading_whitespace and remaining_str[0] == " ")):
             wrapped_strs.append(remaining_str[1 : width + 1].replace("\n", " ") or " ")
             remaining_str = remaining_str[width + 1 :]
         else:
