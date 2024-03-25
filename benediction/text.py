5649ffe195aefd50635388897eabaf875bb653a5
austerj <johanauster@gmail.com>
2024-03-25 19:13:03 +0100

    impr: use dict for str strip

--- benediction/text.py
+++ benediction/text.py
@@ -6,20 +6,20 @@ _XALIGN: dict[HorizontalAlignment, typing.Callable[[list[str], int], list[str]]]
     "center": lambda strs, width: [f"{s:^{width}}" for s in strs],
     "right": lambda strs, width: [f"{s:>{width}}" for s in strs],
 }
+_XSTRIP: dict[HorizontalAlignment, typing.Callable[[list[str]], list[str]]] = {
+    "left": lambda strs: [s.lstrip() for s in strs],
+    "center": lambda strs: [s.strip() for s in strs],
+    "right": lambda strs: [s.rstrip() for s in strs],
+}
 
 
 def align(strs: list[str], alignment: HorizontalAlignment):
     """Horizontal alignment of strings."""
     # strip whitespace
-    if alignment == "left":
-        stripped_strs = [s.lstrip() for s in strs]
-    elif alignment == "right":
-        stripped_strs = [s.rstrip() for s in strs]
-    else:
-        stripped_strs = [s.strip() for s in strs]
-    width = max(len(s) for s in stripped_strs)
+    strs = _XSTRIP[alignment](strs)
+    width = max(len(s) for s in strs)
     # align each row in same width
-    return _XALIGN[alignment](stripped_strs, width)
+    return _XALIGN[alignment](strs, width)
 
 
 def simple_wrap(strs: str | list[str], width: int, ignore_leading_whitespace: bool = True) -> list[str]:
