599c2ad7eb78ee5d41c5c461d96ebb6f7e020325
austerj <johanauster@gmail.com>
2024-04-01 14:57:28 +0200

    feat: infer Node frame as boundary of child nodes

--- benediction/core/layout/node.py
+++ benediction/core/layout/node.py
@@ -8,13 +8,13 @@ from benediction.core.layout.frame import ConstrainedFrame
 
 @dataclass(slots=True, repr=False)
 class Node:
-    """Object that forms a hierarchy of relations to parent and child Nodes."""
+    """Object that forms part of a hierarchy of relations to parent and child Nodes."""
 
     # relational nodes
     parent: Node | None = None
     children: list[Node] = field(default_factory=list)
     # constrained frame
-    _cframe: ConstrainedFrame = field(default_factory=ConstrainedFrame, init=False)
+    cframe: ConstrainedFrame = field(default_factory=ConstrainedFrame, init=False)
 
     def __post_init__(self):
         if self.parent:
@@ -26,7 +26,7 @@ class Node:
     @property
     def frame(self):
         """Inner Frame associated with Node."""
-        return self._cframe.frame
+        return self.cframe.frame
 
     def apply(self, fn: typing.Callable[[Node], typing.Any], depth: int = -1, to_self: bool = True):
         """Apply function to (optional) self and all child nodes recursively."""
@@ -39,8 +39,42 @@ class Node:
         for child in self.children:
             child.apply(fn, depth - 1)
 
-    def flatten(self, depth: int = -1):
-        """Get flattened list of layout nodes."""
+    def flatten(self, depth: int = -1, include_self: bool = True):
+        """Get flattened list of child nodes."""
         nodes = []
-        self.apply(lambda node: nodes.append(node), depth)
+        self.apply(lambda node: nodes.append(node), depth, include_self)
         return nodes
+
+    def update_frame(
+        self,
+        top: int | None = None,
+        left: int | None = None,
+        width: int | None = None,
+        height: int | None = None,
+    ):
+        """Update frame to the smallest region that contains the given dimensions and all child nodes."""
+        # NOTE: default behavior of the base node is a bottoms-up approach - basically, assume the
+        # child nodes have dealt with their frames explicitly and infer the boundary from them,
+        # ignoring all constraints (since we are not working relative to an outer frame)
+        right, bottom = None, None
+
+        def update_boundaries(node: Node):
+            nonlocal top, left, right, bottom
+            frame = node.frame
+            # update running top/left/right/bottom boundaries
+            if frame.is_ready:
+                top = frame.top_abs if top is None or frame.top_abs < top else top
+                left = frame.left_abs if left is None or frame.left_abs < left else left
+                right = frame.right_abs if right is None or frame.right_abs > right else right
+                bottom = frame.bottom_abs if bottom is None or frame.bottom_abs > bottom else bottom
+
+        # update boundaries from recursion through child nodes
+        self.apply(update_boundaries, to_self=False)
+        width = width if right is None or left is None else (right - left) + 1
+        height = height if top is None or bottom is None else (bottom - top) + 1
+
+        if not any((top is None, left is None, width is None, height is None)):
+            # update inner frame directly (ignoring constraints)
+            self.cframe.frame.set_dimensions(top, left, height, width)  # type: ignore
+        else:
+            raise ValueError("WAAH")
--- tests/layout/test_node.py
+++ tests/layout/test_node.py
@@ -22,6 +22,22 @@ def nodes():
     return root
 
 
+@pytest.fixture
+def sized_nodes():
+    # depth 0
+    root = Node()
+    # depth 1
+    node_0 = Node(root)
+    node_0.cframe.frame.set_dimensions(6, 1, 7, 7)
+    # depth 2
+    node_0_0 = Node(node_0)
+    node_0_0.cframe.frame.set_dimensions(1, 2, 5, 5)
+    node_0_1 = Node(node_0)
+    node_0_2 = Node(node_0)
+    node_0_2.cframe.frame.set_dimensions(12, 7, 2, 2)
+    return root
+
+
 def test_apply(nodes):
     # function passed to apply is called recursively by each child node
     i = 0
@@ -40,3 +56,38 @@ def test_flatten(nodes):
     assert len(nodes.flatten(1)) == 3
     assert len(nodes.flatten(2)) == 8
     assert len(nodes.flatten(3)) == 10
+
+
+def test_update_frame(sized_nodes):
+    # NOTE: easiest way to verify the validity of these tests is by writing out the layout in a
+    # spreadsheet and mark each cell with the node that contains it
+
+    # initial frame is not ready
+    assert not sized_nodes.frame.is_ready
+
+    # update dimensions to smallest region that contains all children
+    sized_nodes.update_frame()
+
+    # new dimensions are as expected (containing all children)
+    assert sized_nodes.frame.top_abs == 1
+    assert sized_nodes.frame.left_abs == 1
+    assert sized_nodes.frame.right_abs == 8
+    assert sized_nodes.frame.bottom_abs == 13
+    assert sized_nodes.frame.width_outer == 8
+    assert sized_nodes.frame.height_outer == 13
+
+    # updating frame of depth 1 node overwrites the existing frame
+    node_0 = sized_nodes.children[0]
+    node_0.update_frame()
+    assert node_0.frame.top_abs == 1
+    assert node_0.frame.left_abs == 2
+    assert node_0.frame.right_abs == 8
+    assert node_0.frame.bottom_abs == 13
+    assert node_0.frame.width_outer == 7
+    assert node_0.frame.height_outer == 13
+
+    # original frame hasn't been updated yet
+    assert sized_nodes.frame.width_outer == 8
+    # after updating it will match the depth 1 boundary
+    sized_nodes.update_frame()
+    assert sized_nodes.frame.width_outer == 7
