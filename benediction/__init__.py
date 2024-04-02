efb1261198f9fc79cc22fc893c51cef4759baee3
austerj <johanauster@gmail.com>
2024-04-02 21:12:11 +0200

    fix: imports

--- benediction/__init__.py
+++ benediction/__init__.py
@@ -1,4 +1,5 @@
 from benediction.app import Application
 from benediction.screen import Screen
 from benediction.style import Color, ColorPair
-from benediction.window import Pad, ScrollingPad, Window
+from benediction.core.window import Pad, ScrollingPad, Window
+from benediction.core.node.layout.builder import Layout
