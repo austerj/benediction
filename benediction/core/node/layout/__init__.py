9653e777b33dcd9df81c8e302b12c9b9b97989f7
austerj <johanauster@gmail.com>
2024-04-02 16:51:02 +0200

    fix: mutable defaults (rookie mistake)

--- benediction/core/node/layout/__init__.py
+++ benediction/core/node/layout/__init__.py
@@ -6,14 +6,14 @@ from benediction.core.node.spec import ColumnSpecKwargs, NodeSpecKwargs, RowSpec
 
 
 # wrapper to pass kwargs as dict to LayoutNode constructor
-def LayoutNode(children: list[Node] = [], is_row: bool = True, **kwargs: typing.Unpack[NodeSpecKwargs]):
-    return _LayoutNode(children, is_row=is_row, spec_kwargs=kwargs)
+def LayoutNode(children: list[Node] | None = None, is_row: bool = True, **kwargs: typing.Unpack[NodeSpecKwargs]):
+    return _LayoutNode(children if children is not None else [], is_row=is_row, spec_kwargs=kwargs)
 
 
 # wrappers for LayoutNode constructor with initial row / column config and appropriate kwarg hints
-def Row(children: list[Node] = [], **kwargs: typing.Unpack[RowSpecKwargs]):
-    return LayoutNode(children, is_row=True, **kwargs)
+def Row(children: list[Node] | None = None, **kwargs: typing.Unpack[RowSpecKwargs]):
+    return LayoutNode(children if children is not None else [], is_row=True, **kwargs)
 
 
-def Column(children: list[Node] = [], **kwargs: typing.Unpack[ColumnSpecKwargs]):
-    return LayoutNode(children, is_row=False, **kwargs)
+def Column(children: list[Node] | None = None, **kwargs: typing.Unpack[ColumnSpecKwargs]):
+    return LayoutNode(children if children is not None else [], is_row=False, **kwargs)
