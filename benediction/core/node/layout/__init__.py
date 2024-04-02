b03baf8cb9776ca6f8ad52e3eb6b2c3f261a27c0
austerj <johanauster@gmail.com>
2024-04-02 17:24:42 +0200

    rm: LayoutNode constructor wrapper

--- benediction/core/node/layout/__init__.py
+++ benediction/core/node/layout/__init__.py
@@ -1,19 +1,14 @@
 import typing
 
-from benediction.core.node.layout.layout import LayoutNode as _LayoutNode
+from benediction.core.node.layout.layout import LayoutNode
 from benediction.core.node.node import Node
-from benediction.core.node.spec import ColumnSpecKwargs, NodeSpecKwargs, RowSpecKwargs
-
-
-# wrapper to pass kwargs as dict to LayoutNode constructor
-def LayoutNode(children: list[Node] | None = None, is_row: bool = True, **kwargs: typing.Unpack[NodeSpecKwargs]):
-    return _LayoutNode(children if children is not None else [], is_row=is_row, spec_kwargs=kwargs)
+from benediction.core.node.spec import ColumnSpecKwargs, RowSpecKwargs
 
 
 # wrappers for LayoutNode constructor with initial row / column config and appropriate kwarg hints
 def Row(children: list[Node] | None = None, **kwargs: typing.Unpack[RowSpecKwargs]):
-    return LayoutNode(children if children is not None else [], is_row=True, **kwargs)
+    return LayoutNode(children if children is not None else [], is_row=True, spec_kwargs=kwargs)  # type: ignore
 
 
 def Column(children: list[Node] | None = None, **kwargs: typing.Unpack[ColumnSpecKwargs]):
-    return LayoutNode(children if children is not None else [], is_row=False, **kwargs)
+    return LayoutNode(children if children is not None else [], is_row=False, spec_kwargs=kwargs)  # type: ignore
